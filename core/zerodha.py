a8d67d1b0cc5d886b7d7031d0bf5e4181c3f81fd
mannvirk <mannvirk@gmail.com>
2026-02-10 15:05:39 +0530

    Add files via upload

--- core/zerodha.py
+++ core/zerodha.py
@@ -2,8 +2,8 @@ from kiteconnect import KiteConnect
 import streamlit as st
 
 def get_kite():
-    api_key = st.secrets["KITE_API_KEY"]
-    access_token = st.secrets["KITE_ACCESS_TOKEN"]
+    api_key = st.secrets[" 1jahcv7cqzcrgw17"]
+    access_token = st.secrets["plylgLwOi5ioo2nkqBh0VRyw1DYzKHnp"]
 
     kite = KiteConnect(api_key=api_key)
     kite.set_access_token(access_token)
