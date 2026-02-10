232ac6700d9d68611891405a2c2fbe2ced9e6fe8
mannvirk <mannvirk@gmail.com>
2026-02-10 16:35:27 +0530

    Update zerodha.py

--- core/zerodha.py
+++ core/zerodha.py
@@ -3,9 +3,10 @@ import streamlit as st
 
 def get_kite():
     api_key = st.secrets["1jahcv7cqzcrgw17"]
-    access_token = st.secrets["plylgLwOi5ioo2nkqBh0VRyw1DYzKHnp"]
+    access_token = st.secrets["RmerrnO3SlXjIQmaEQ5y22Su0rrJjET2"]
 
     kite = KiteConnect(api_key=api_key)
     kite.set_access_token(access_token)
     return kite
+
 
